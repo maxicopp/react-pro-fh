57b22e1d90177539eccf6c2204ae5d3bb6de5398
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-02 04:44:00 -0300

    Parametrizar y pulir nuestro custom hook

--- 01-bases/src/bases/CounterHook.tsx
+++ 01-bases/src/bases/CounterHook.tsx
@@ -1,12 +1,12 @@
 import { useCounter } from '../hooks/useCounter';
 
 export const CounterHook = () => {
-    const { counterElement, counter, handleClick } = useCounter();
+    const { elementToAnimate, counter, handleClick } = useCounter({ maxCount: 15 });
 
     return (
         <>
             <h1>CounterHook:</h1>
-            <h2 ref={counterElement}>{counter}</h2>
+            <h2 ref={elementToAnimate}>{counter}</h2>
             <button onClick={handleClick}>+1</button>
         </>
     )
