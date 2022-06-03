0361eb6813abe50a2121e885d3c5cfa72bee5947
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-03 13:22:15 -0300

    IncreaseBy - Action

--- 01-bases/src/bases/CounterReducer.tsx
+++ 01-bases/src/bases/CounterReducer.tsx
@@ -7,20 +7,27 @@ interface CounterState {
 }
 
 const INITIAL_STATE: CounterState = {
-    counter: 10,
-    previous: 15,
-    changes: 20,
+    counter: 0,
+    previous: 0,
+    changes: 0,
 }
 
 type CounterAction = { type: 'increaseBy', payload: { value: number } } | { type: 'reset' };
 
 const counterReducer = (state: CounterState, action: CounterAction): CounterState => {
+    const { counter, changes } = state;
     switch (action.type) {
         case 'reset':
             return {
                 counter: 0,
                 previous: 0,
                 changes: 0,
+            }
+        case 'increaseBy':
+            return {
+                counter: counter + action.payload.value,
+                previous: counter,
+                changes: changes + 1
             }
 
         default:
@@ -29,12 +36,19 @@ const counterReducer = (state: CounterState, action: CounterAction): CounterStat
 }
 
 export const CounterReducerComponent = () => {
-    const [{ counter }, dispatch] = useReducer(counterReducer, INITIAL_STATE)
-    const handleClick = () => dispatch({ type: 'reset' });
+    const [counterState, dispatch] = useReducer(counterReducer, INITIAL_STATE)
+    const handleReset = () => dispatch({ type: 'reset' });
+    const increaseBy = (value: number) => dispatch({ type: 'increaseBy', payload: { value } });
     return (
         <>
-            <h1>Counter Reducer: {counter}</h1>
-            <button onClick={handleClick}>Reset</button>
+            <h1>Counter Reducer</h1>
+            <pre>
+                {JSON.stringify(counterState, null, 2)}
+            </pre>
+            <button onClick={() => increaseBy(1)}>+1</button>
+            <button onClick={() => increaseBy(5)}>+5</button>
+            <button onClick={() => increaseBy(10)}>+10</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     )
 }
