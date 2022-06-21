4ddcae34b1568bda301263373ee9a2d7c26ebbf4
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-21 00:48:51 -0300

    Cambiar el nombre de los chunks

--- 02-react-app-v5/src/routes/routes.ts
+++ 02-react-app-v5/src/routes/routes.ts
@@ -4,31 +4,40 @@ import { lazy, LazyExoticComponent } from 'react';
 
 type JSXComponent = () => JSX.Element;
 
-const LazyPage1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const LazyPage2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const LazyPage3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const LazyPage1 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1')
+);
+const LazyPage2 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
+);
+const LazyPage3 = lazy(
+  () =>
+    import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+);
 
 interface Route {
-    path: string;
-    Component: LazyExoticComponent<JSXComponent> | JSXComponent;
-    name: string;
-    children?: Route[];
+  path: string;
+  Component: LazyExoticComponent<JSXComponent> | JSXComponent;
+  name: string;
+  children?: Route[];
 }
 
 export const routes: Route[] = [
-    {
-        path: '/lazy1',
-        Component: LazyPage1,
-        name: 'LazyPage-1'
-    },
-    {
-        path: '/lazy2',
-        Component: LazyPage2,
-        name: 'LazyPage-2'
-    },
-    {
-        path: '/lazy3',
-        Component: LazyPage3,
-        name: 'LazyPage-3'
-    },
+  {
+    path: "/lazy1",
+    Component: LazyPage1,
+    name: "LazyPage-1",
+  },
+  {
+    path: "/lazy2",
+    Component: LazyPage2,
+    name: "LazyPage-2",
+  },
+  {
+    path: "/lazy3",
+    Component: LazyPage3,
+    name: "LazyPage-3",
+  },
 ];
