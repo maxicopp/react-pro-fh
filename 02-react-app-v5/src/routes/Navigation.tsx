ea1881a64d29c2bff763d62a95e109136d6c23fd
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-27 13:50:15 -0300

    Implementar las rutas hijas

--- 02-react-app-v5/src/routes/Navigation.tsx
+++ 02-react-app-v5/src/routes/Navigation.tsx
@@ -21,7 +21,7 @@ export const Navigation = () => {
               {
                 routes.map(({ name, path }) => (
                   <li key={path}>
-                    <NavLink to={path} activeClassName="nav-active" exact>{name}</NavLink>
+                    <NavLink to={path} activeClassName="nav-active">{name}</NavLink>
                   </li>
                 ))
               }
