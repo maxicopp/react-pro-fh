c8f22edef445238d99b601ba381e7e7b9e484e65
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-29 23:45:19 -0300

    Compound Component Pattern - Segundo paso

--- 03-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/components/ProductCard.tsx
@@ -1,9 +1,11 @@
+import { ReactElement } from 'react';
 import styles from '../styles/styles.module.css';
 import noImage from '../assets/no-image.jpg';
 import { useProduct } from '../hooks/useProduct';
 
 interface Props {
     product: Product;
+    children?: ReactElement | ReactElement[];
 }
 interface Product {
     id: string;
@@ -38,13 +40,14 @@ export const ProductButtons = ({ counter, increaseBy }: ProductButtonProps) => {
     )
 }
 
-export const ProductCard = ({ product }: Props) => {
+export const ProductCard = ({ children, product }: Props) => {
     const { counter, increaseBy } = useProduct();
     return (
         <div className={styles.productCard}>
-            <ProductImage img={product.img} />
+            {children}
+            {/* <ProductImage img={product.img} />
             <ProductTitle title={product.title} />
-            <ProductButtons counter={counter} increaseBy={increaseBy} />
+            <ProductButtons counter={counter} increaseBy={increaseBy} /> */}
         </div>
     );
 };
--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard } from '../components/ProductCard';
+import { ProductCard, ProductImage, ProductTitle } from '../components/ProductCard';
 
 const product = {
   id: '1',
@@ -12,7 +12,10 @@ export const ShoppingPage = () => {
         <h1>Shopping Store</h1>
         <hr />
         <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-          <ProductCard product={product} />
+          <ProductCard product={product}>
+            <ProductImage img={product.img} />
+            <ProductTitle title={'Hello world'} />
+          </ProductCard>
         </div>
     </div>
   )
