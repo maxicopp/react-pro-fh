5fafdf41668482e4050f656d031181205e1a0f24
Maximiliano Cóppola <maxicopp@outlook.com>
2022-06-29 23:59:58 -0300

    Unificar exportación de componentes

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,4 +1,4 @@
-import { ProductCard, ProductImage, ProductTitle } from '../components/ProductCard';
+import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components/ProductCard';
 
 const product = {
   id: '1',
@@ -9,14 +9,24 @@ const product = {
 export const ShoppingPage = () => {
   return (
     <div>
-        <h1>Shopping Store</h1>
-        <hr />
-        <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-          <ProductCard product={product}>
-            <ProductImage img={product.img} />
-            <ProductTitle title={'Hello world'} />
-          </ProductCard>
-        </div>
+      <h1>Shopping Store</h1>
+      <hr />
+      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+        <ProductCard product={product}>
+          <ProductCard.Image img={product.img} />
+          <ProductCard.Title title={'Hello world'} />
+          <ProductCard.Buttons counter={0} increaseBy={function (value: number): void {
+            throw new Error('Function not implemented.');
+          }} />
+        </ProductCard>
+        <ProductCard product={product}>
+          <ProductImage img={product.img} />
+          <ProductTitle title={'Hello world'} />
+          <ProductButtons counter={0} increaseBy={function (value: number): void {
+            throw new Error('Function not implemented.');
+          }} />
+        </ProductCard>
+      </div>
     </div>
   )
 }
