a98b3eaf59ca09c1beca5fed32d1778a93026dc8
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-09 12:47:19 -0300

    Estado del carrito de compras

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 import { Product } from '../interfaces/interfaces';
 import '../styles/custom-styles.css';
@@ -15,7 +16,12 @@ const product2 = {
 
 const products: Product[] = [product1, product2];
 
+interface ProductInCart extends Product {
+  count: number;
+}
+
 export const ShoppingPage = () => {
+  const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
   return (
     <div>
       <h1>Shopping Store</h1>
