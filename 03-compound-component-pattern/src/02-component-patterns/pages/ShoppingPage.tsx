0716b657be301ff7914598c71eba66d62811eb67
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-09 12:18:10 -0300

    Continuación de la sección - Control Props

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -6,6 +6,11 @@ const product = {
   title: 'Coffee Mug - Card',
   img: './coffee-mug.png'
 }
+const product2 = {
+  id: '2',
+  title: 'Coffee Mug - Meme',
+  img: './coffee-mug2.png'
+}
 
 export const ShoppingPage = () => {
   return (
@@ -18,15 +23,10 @@ export const ShoppingPage = () => {
           <ProductCard.Title className="text-bold" />
           <ProductCard.Buttons className="custom-buttons" />
         </ProductCard>
-        <ProductCard product={product} className="bg-dark text-white">
+        <ProductCard product={product2} className="bg-dark text-white">
           <ProductImage className="custom-image" />
           <ProductTitle className="text-bold" />
           <ProductButtons className="custom-buttons" />
-        </ProductCard>
-        <ProductCard product={product} style={{ backgroundColor: '#70D1F8' }}>
-          <ProductImage style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }} />
-          <ProductTitle style={{ fontWeight: 'bold' }} />
-          <ProductButtons style={{ display: 'flex', justifyContent: 'end' }} />
         </ProductCard>
       </div>
     </div>
