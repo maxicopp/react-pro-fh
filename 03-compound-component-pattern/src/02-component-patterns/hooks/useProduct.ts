3875dd7609c4286cc3ade6ac2775c712d01e01f7
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-13 18:20:25 -0300

    Control Props

--- 03-compound-component-pattern/src/02-component-patterns/hooks/useProduct.ts
+++ 03-compound-component-pattern/src/02-component-patterns/hooks/useProduct.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { onChangeArgs, Product } from '../interfaces/interfaces';
 interface useProductArgs {
   product: Product;
@@ -12,7 +12,11 @@ export const useProduct = ({
   value = 0,
 }: useProductArgs) => {
   const [counter, setCounter] = useState(value);
+  const isControlled = useRef(!!onChange);
   const increaseBy = (value: number) => {
+    if (isControlled.current) {
+      return onChange!({ count: value, product });
+    }
     const newValue = Math.max(counter + value, 0);
     setCounter(newValue);
     onChange && onChange({ count: newValue, product });
