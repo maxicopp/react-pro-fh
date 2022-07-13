4983736f19212840ddd1e0c2e48881da5f3e8ae2
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-13 19:05:19 -0300

    Controlando el estado desde el padre

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -23,13 +23,24 @@ interface ProductInCart extends Product {
 export const ShoppingPage = () => {
   const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
   const onProductCountChange = ({ count, product }: { count: number; product: Product }) => {
-    console.log({ count })
     setShoppingCart(oldShoppingCart => {
-      if (count === 0) {
-        const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-        return rest;
+      const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+
+      if (Math.max(productInCart.count + count, 0) > 0) {
+        productInCart.count += count;
+        return {
+          ...oldShoppingCart,
+          [product.id]: productInCart
+        }
       }
-      return { ...oldShoppingCart, [product.id]: { ...product, count } };
+
+      const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+      return rest;
+      // if (count === 0) {
+      //   const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+      //   return rest;
+      // }
+      // return { ...oldShoppingCart, [product.id]: { ...product, count } };
     })
   }
   return (
