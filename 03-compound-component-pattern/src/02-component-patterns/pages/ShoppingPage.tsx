3387ec67c8c4b3b1292f502e5a616ce2269728af
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-17 15:58:58 -0300

    Continuación de la sección

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,35 +1,19 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
-import { useShoppingCart } from '../hooks/useShoppingCart';
 import { products } from '../data/products';
 import '../styles/custom-styles.css';
 
+const product = products[0];
+
 export const ShoppingPage = () => {
-  const { shoppingCart, onProductCountChange } = useShoppingCart();
   return (
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        {
-          products.map(product => (
-            <ProductCard value={shoppingCart[product.id]?.count || 0} product={product} className="bg-dark text-white" key={product.id} onChange={onProductCountChange}>
-              <ProductImage className="custom-image" />
-              <ProductTitle className="text-bold" />
-              <ProductButtons className="custom-buttons" />
-            </ProductCard>
-          ))
-        }
-      </div>
-      <div className="shopping-cart">
-        {
-          Object.entries(shoppingCart).map(([key, product]) => (
-            <ProductCard value={product.count} key={key} product={product} className="bg-dark text-white" style={{ width: '100px' }} onChange={onProductCountChange}>
-              <ProductImage className="custom-image" />
-              <ProductButtons className="custom-buttons" style={{ display: 'flex', justifyContent: 'center' }} />
-            </ProductCard>
-          ))
-        }
-      </div>
+      <ProductCard product={product} className="bg-dark text-white">
+        <ProductImage className="custom-image" />
+        <ProductTitle className="text-bold" />
+        <ProductButtons className="custom-buttons" />
+      </ProductCard>
     </div>
   )
 }
