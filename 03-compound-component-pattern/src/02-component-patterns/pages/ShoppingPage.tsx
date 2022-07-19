af98aeb8e3e52165facbd65f2453e1e84a9a2b0e
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-19 00:43:49 -0300

    Preparación de nuestro juego de componentes

--- 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
+++ 03-compound-component-pattern/src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,6 +1,5 @@
 import { ProductButtons, ProductCard, ProductImage, ProductTitle } from '../components';
 import { products } from '../data/products';
-import '../styles/custom-styles.css';
 
 const product = products[0];
 
@@ -9,20 +8,13 @@ export const ShoppingPage = () => {
     <div>
       <h1>Shopping Store</h1>
       <hr />
-      <ProductCard key={product.id} product={product} className="bg-dark text-white" initialValues={{ count: 4, maxCount: 10 }}>
+      <ProductCard key={product.id} product={product} initialValues={{ count: 4, maxCount: 10 }}>
         {
           ({ increaseBy, count, isMaxCountReached, reset, maxCount }) => (
             <>
-              <ProductImage className="custom-image" />
-              <ProductTitle className="text-bold" />
-              <ProductButtons className="custom-buttons" />
-              <button onClick={reset}>Reset</button>
-              <button onClick={() => increaseBy(-2)}>-2</button>
-              {
-                !isMaxCountReached &&
-                <button onClick={() => increaseBy(2)}>+2</button>
-              }
-              <span>{count} - {maxCount}</span>
+              <ProductImage />
+              <ProductTitle />
+              <ProductButtons />
             </>
           )
         }
