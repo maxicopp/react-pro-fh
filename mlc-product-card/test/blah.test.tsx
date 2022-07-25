d5ad68b80f9728bb468df4c43344e6f23b26b8ea
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-25 18:20:41 -0300

    Versión 1.0.0-rc lista

--- mlc-product-card/test/blah.test.tsx
+++ mlc-product-card/test/blah.test.tsx
@@ -1,11 +1,8 @@
-// import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    // ReactDOM.render(<Thing />, div);
     ReactDOM.unmountComponentAtNode(div);
-    console.log('Hola mundo');
   });
 });
