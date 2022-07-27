be64a54e04d8a499418dc72911e2aa398240d2cd
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-27 15:41:44 -0300

    Expandir funcionalidad de nuestro custom hook

--- 02-react-app/src/03-forms/hooks/useForm.ts
+++ 02-react-app/src/03-forms/hooks/useForm.ts
@@ -8,5 +8,11 @@ export const useForm = <T>(initState: T) => {
       [event.target.name]: event.target.value,
     }));
   };
-  return { ...formData, formData, onChange };
+  const resetForm = () => setFormData({ ...initState });
+  const isValidEmail = (email: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  };
+  return { ...formData, formData, onChange, resetForm, isValidEmail };
 };
