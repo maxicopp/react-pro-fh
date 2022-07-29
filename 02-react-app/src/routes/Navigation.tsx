a7efa622eebd80b66d790e81b9b1aac5f66a930d
Maximiliano Cóppola <maxicopp@outlook.com>
2022-07-29 16:32:26 -0300

    Yup - ValidationSchema Builder

--- 02-react-app/src/routes/Navigation.tsx
+++ 02-react-app/src/routes/Navigation.tsx
@@ -5,6 +5,7 @@ import {
   NavLink
 } from 'react-router-dom';
 import { FormikBasicPage } from '../03-forms/pages/FormikBasicPage';
+import { FormikYupPage } from '../03-forms/pages/FormikYupPage';
 import { RegisterPage } from '../03-forms/pages/RegisterPage';
 
 import logo from '../logo.svg';
@@ -23,6 +24,9 @@ export const Navigation = () => {
               <NavLink to="/formik-basic" activeClassName="nav-active" exact>Formik Basic</NavLink>
             </li>
             <li>
+              <NavLink to="/formik-yup" activeClassName="nav-active" exact>Formik Yup</NavLink>
+            </li>
+            <li>
               <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
             </li>
           </ul>
@@ -36,6 +40,9 @@ export const Navigation = () => {
           </Route>
           <Route path="/formik-basic">
             <FormikBasicPage />
+          </Route>
+          <Route path="/formik-yup">
+            <FormikYupPage />
           </Route>
           <Route path="/">
             <h1>Home</h1>
