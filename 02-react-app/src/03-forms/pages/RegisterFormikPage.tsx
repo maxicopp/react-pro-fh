6985f0c6974f70f8a5f1cb306267124d3c44b85e
Maximiliano Cóppola <maxicopp@outlook.com>
2022-08-01 15:27:52 -0300

    Formulario de registro con Formik

--- 02-react-app/src/03-forms/pages/RegisterFormikPage.tsx
+++ 02-react-app/src/03-forms/pages/RegisterFormikPage.tsx
@@ -1,35 +1,46 @@
-import { FormEvent } from 'react';
-import { useForm } from '../hooks/useForm';
+import { Formik, Form } from 'formik';
+import * as Yup from 'yup';
+import { MyTextInput } from '../components';
+
 import '../styles/styles.css';
 
 export const RegisterFormikPage = () => {
-    const { formData, onChange, resetForm, isValidEmail, name, email, password1, password2 } = useForm({
-        name: 'Maxi',
-        email: 'algo@algo.com',
-        password1: '',
-        password2: ''
-    });
-    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        console.log(formData);
-    };
     return (
         <div>
             <h1>Register Formik Page</h1>
-            <form noValidate onSubmit={onSubmit} >
-                <input type="text" placeholder="Name" name="name" value={name} onChange={onChange} className={`${name.trim().length <= 0 && 'has-error'}`} />
-                {name.trim().length <= 0 && <span>Este campo es necesario</span>}
-                <input type="email" placeholder="Email" name="email" value={email} onChange={onChange} className={`${!isValidEmail(email) && 'has-error'}`} />
-                {!isValidEmail(email) && <span>Email no es válido</span>}
-                <input type="password" placeholder="Password" name="password1" value={password1} onChange={onChange} />
-                {password1.trim().length <= 0 && <span>Este campo es necesario</span>}
-                {password1.trim().length < 6 && password1.trim().length > 0 && <span>La contraseña tiene que tener 6 letras</span>}
-                <input type="password" placeholder="Repeat Password" name="password2" value={password2} onChange={onChange} />
-                {password2.trim().length <= 0 && <span>Este campo es necesario</span>}
-                {password2.trim().length > 0 && password1 !== password2 && <span>Las contraseñas deben de ser iguales</span>}
-                <button type="submit">Create</button>
-                <button type="button" onClick={resetForm}>Reset Form</button>
-            </form>
+            <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                    password1: '',
+                    password2: '',
+                }}
+                onSubmit={values => console.log(values)}
+                validationSchema={Yup.object({
+                    name: Yup.string().min(2, 'El nombre debe de ser de 3 caracteres o más').max(15, 'El nombre debe de ser menor de 15 caracteres').required('Requerido'),
+                    email: Yup.string().email('Revise el formato del correo').required('Requerido'),
+                    password1: Yup.string().min(6, 'Mínimo 6 letras').required('Requerido'),
+                    password2: Yup.string().oneOf([Yup.ref('password1')], 'Las contraseñas no son iguales').min(6, 'Mínimo 6 letras').required('Requerido'),
+                })}
+            >
+                {
+                    ({ handleReset }) => (
+                        <Form>
+                            <MyTextInput label="First Name" name="name" placeholder="Maximiliano" />
+
+                            <MyTextInput label="Email Address" name="email" placeholder="john@google.com" type="email" />
+
+                            <MyTextInput label="Password" name="password1" placeholder="******" type="password" />
+
+                            <MyTextInput label="Confirm Password" name="password2" placeholder="******" type="password" />
+
+                            <button type="submit">Submit</button>
+                            <button onClick={handleReset}>Reset</button>
+                        </Form>
+                    )
+                }
+            </Formik>
+
         </div>
     )
 }
